d53150c152898cc2c44fa2b4cf7344880dfaa536
Saul Garcia <garcialuevanosaul@gmail.com>
2026-03-13 15:51:22 -0600

    Meta Pixel Code added in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const montserrat = Montserrat({
@@ -34,6 +35,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={montserrat.variable}>
       <body suppressHydrationWarning className="min-h-screen font-sans antialiased">
+        <Script id="meta-pixel-script" dangerouslySetInnerHTML={{ __html: "!function(f,b,e,v,n,t,s) {if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)}(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '927301083105505'); fbq('track', 'PageView');" }} />
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <noscript><img height="1" width="1" style={{ display: 'none' }} src="https://www.facebook.com/tr?id=927301083105505&ev=PageView&noscript=1" alt="" /></noscript>
         {children}
       </body>
     </html>
