a81258f42b2d6eed582ef12e8be9041f9d3d14f6
Moyer0 <moyersalcido@gmail.com>
2026-04-13 17:29:08 -0600

    Sell Fixed
    Fix

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -220,7 +220,7 @@ export function SiteHeader({
                 )}
                 aria-label="Home"
               >
-                HOME
+                Hom
               </Link>
 
               <nav aria-label="Primary right" className="flex-1">
