72310409fdd070ae2fc2d9a9601ba1b1627bf9ee
WarrenGoodellV <Waelgoodell@gmail.com>
2026-08-17 15:38:35 -0600

    New Google Analytics
    Update google analytics to match new id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import Script from "next/script";
-import { GoogleAnalytics } from "@next/third-parties/google";
 import { FloatingWhatsAppBubble } from "@/components/FloatingWhatsAppBubble";
 import "./globals.css";
 
@@ -36,6 +35,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={montserrat.variable}>
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-2NGQC6ZEXR"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+
+
+              gtag('js', new Date());
+              gtag('config', 'G-2NGQC6ZEXR');
+            `,
+          }}
+        />
+      </head>
       <body suppressHydrationWarning className="min-h-screen font-sans antialiased">
         <Script id="meta-pixel-script" dangerouslySetInnerHTML={{ __html: "!function(f,b,e,v,n,t,s) {if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)}(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '927301083105505'); fbq('track', 'PageView');" }} />
         {/* eslint-disable-next-line @next/next/no-img-element */}
@@ -43,7 +57,6 @@ export default function RootLayout({
         {children}
         <FloatingWhatsAppBubble />
       </body>
-      <GoogleAnalytics gaId="G-3Q7SVSWGDM" />
     </html>
   );
 }
