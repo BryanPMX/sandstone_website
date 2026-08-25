a6032ecae9c90092d653c4a17bf7b3e1b450f711
sandstoneep-art <sandstoneep@gmail.com>
2026-08-25 16:23:00 -0600

    chore: update Meta Pixel ID to Sandstone's own pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
           id="meta-pixel-script"
           dangerouslySetInnerHTML={{
             __html:
-              "!function(f,b,e,v,n,t,s) {if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)}(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '927301083105505'); fbq('track', 'PageView');",
+              "!function(f,b,e,v,n,t,s) {if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)}(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '1033581349513770'); fbq('track', 'PageView');",
           }}
         />
 
@@ -71,7 +71,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=927301083105505&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1033581349513770&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
